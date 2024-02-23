53b1d238d7f053b90a3fdc5a71c78466e15ee1f9
siddhantmaharana <siddhantmaharana14@gmail.com>
2024-02-23 11:13:24 -0500

    modified about me

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const currentTime = new Date();
+// const currentTime = new Date();
 
-const day = new Date();
-day.setHours(08, 0o0, 0);
-const night = new Date();
-night.setHours(18, 0o0, 0);
+// const day = new Date();
+// day.setHours(08, 0o0, 0);
+// const night = new Date();
+// night.setHours(18, 0o0, 0);
+// const body = document.body;
+
+// function setMode() {
+// 	if (currentTime <= day || currentTime >= night) {
+// 		body.classList.add('dark-mode');
+// 	} else {
+// 		body.classList.remove('dark-mode');
+// 	}
+// }
+// setInterval(function () {
+// 	setMode();
+// }, 1000 * 60 * 5);
+
+// setMode();
+
+// DARK MODE ACTIVATED- FOREVER!!
 const body = document.body;
-
-function setMode() {
-	if (currentTime <= day || currentTime >= night) {
-		body.classList.add('dark-mode');
-	} else {
-		body.classList.remove('dark-mode');
-	}
-}
-setInterval(function () {
-	setMode();
-}, 1000 * 60 * 5);
-
-setMode();
+body.classList.add('dark-mode');
